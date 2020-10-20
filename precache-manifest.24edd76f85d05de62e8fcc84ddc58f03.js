8f32e8e8fd4afa087d29ba70921c4b051e41b28f
Kuldeep K Srivastava <kk23907.me@gmail.com>
2020-10-21 01:02:53 +0530

    Updates

---
+++ precache-manifest.24edd76f85d05de62e8fcc84ddc58f03.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba396ae0c38b914993f813e9e596fe41",
+    "url": "/hackathon/client/index.html"
+  },
+  {
+    "revision": "c1878e27eeee61ef9357",
+    "url": "/hackathon/client/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "841f88403731f20a3d44",
+    "url": "/hackathon/client/static/js/2.bb4b8d4e.chunk.js"
+  },
+  {
+    "revision": "c1878e27eeee61ef9357",
+    "url": "/hackathon/client/static/js/main.95372533.chunk.js"
+  },
+  {
+    "revision": "ae2413abb766e4de8837",
+    "url": "/hackathon/client/static/js/runtime-main.234f74a1.js"
+  },
+  {
+    "revision": "2aa19298b72c4eaa0c39397d1f57989b",
+    "url": "/hackathon/client/static/media/machupicchu.2aa19298.png"
+  },
+  {
+    "revision": "6291845e49aebb9acc3d0d65f8860b08",
+    "url": "/hackathon/client/static/media/metamask.6291845e.jpg"
+  },
+  {
+    "revision": "04610202fa9d1af8817662cfa7052eef",
+    "url": "/hackathon/client/static/media/portis.04610202.png"
+  },
+  {
+    "revision": "cf41dbb52d9f89fc922ff9cc53a59ab4",
+    "url": "/hackathon/client/static/media/rupeetoken.cf41dbb5.jpg"
+  }
+]);
